f9c52a75db24352750fc77ff732939748dd049f3
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-01 16:22:15 -0300

    codigo

--- fibonacci.py
+++ fibonacci.py
@@ -5,7 +5,23 @@
 #un ciclo While True:
 #a la variable c sumarle los valores de los indices 0 y 1 ->
 #<-de la lista unitlizando las variables b y d para acceder a los indices
+#un condicional if para determinar si "c" ->
+#<-es mayor a 1000000 si es mayor realizar un break
 #agregar al final de la lista "c"
 #sumarle 1 a la variable b
 #sumarle 1 a la variable d
 #mostrar la lista fuara del ciclo while
+
+a = [0, 1]
+b = 0
+d = 1
+c = 0
+while True:
+    c = a[b]+a[d]
+    if c > 1000000:
+        break
+    a.append(c)
+    b = b+1
+    d = d+1
+
+print a
