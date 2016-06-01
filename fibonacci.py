78901fd8e99b33dfdaef066cea5ef0536a78adfc
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-01 16:17:23 -0300

    pseudocodigo

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+#inicilizar una lista con el 0 y el 1
+#inicializar una variable b con 0
+#inicializar una variable d con 1
+#inicializar una variable c con 0
+#un ciclo While True:
+#a la variable c sumarle los valores de los indices 0 y 1 ->
+#<-de la lista unitlizando las variables b y d para acceder a los indices
+#agregar al final de la lista "c"
+#sumarle 1 a la variable b
+#sumarle 1 a la variable d
+#mostrar la lista fuara del ciclo while
